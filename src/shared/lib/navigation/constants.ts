40a92184937fc0998002806c69f3eb69f8a04de3
TretyakWeb <tretyakbro@gmail.com>
2023-10-01 22:41:39 +0300

    feat(navigation): add navigation constants

---
+++ src/shared/lib/navigation/constants.ts
@@ -0,0 +1,13 @@
+/**
+ * @typedef {import('@react-navigation/native-stack').NativeStackNavigationOptions} StackNavigationOptions
+ */
+
+export enum screenNames {
+  AllExpenses = "AllExpenses",
+  RecentExpenses = "RecentExpenses",
+  ManageExpense = "ManageExpense",
+}
+
+export const stackScreenOptions = {};
+
+export const tabScreenOptions = {};
