fde508d0ed498265aa58eeedf5af6ec1e075ee66
TretyakWeb <tretyakbro@gmail.com>
2023-10-01 22:42:11 +0300

    feat(styles): add color constants

---
+++ src/shared/lib/styles/colors.ts
@@ -0,0 +1,41 @@
+interface Colors {
+  primary: {
+    300: string;
+    500: string;
+    600: string;
+    700: string;
+    900: string;
+  };
+  error: {
+    100: string;
+    500: string;
+  };
+  gray: {
+    300: string;
+    500: string;
+    900: string;
+  };
+  black: string;
+  white: string;
+}
+
+export const colors: Colors = {
+  primary: {
+    300: "#cac7ff",
+    500: "#514ad5",
+    600: "#4039d4",
+    700: "#2d25d4",
+    900: "#060517",
+  },
+  error: {
+    100: "#ff8785",
+    500: "#d92b27",
+  },
+  gray: {
+    300: "#e5e3e6",
+    500: "#9f9f9f",
+    900: "#121617",
+  },
+  black: "#000",
+  white: "#fff",
+};
