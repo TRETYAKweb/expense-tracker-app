e1924fbc878dfe8f35b264bcb786faaa90cb5a2d
TretyakWeb <tretyakbro@gmail.com>
2023-10-04 11:01:57 +0300

    feat(app): add font loading and SplashScreen handling
    Added font loading using `expo-font` and SplashScreen handling for a better user experience on app start.
    
    - Imported `useFonts` and `fontAssets` from "../shared/lib".
    - Used `SplashScreen.preventAutoHideAsync` to prevent auto-hiding of the splash screen.
    - Implemented `useEffect` to hide the splash screen when fonts are loaded.
    - Added a condition to return `null` while fonts are loading.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Routing } from "./routes/routes";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { fontAssets } from "../shared/lib";
 
 export const App: React.FC = () => {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts(fontAssets);
+
+  useEffect(() => {
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
     <>
       <StatusBar style="light" />
