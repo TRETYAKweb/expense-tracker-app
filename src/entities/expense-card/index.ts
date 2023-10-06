32b476017017df943a597e23f2b3174e466c20d5
TretyakWeb <tretyakbro@gmail.com>
2023-10-06 22:58:34 +0300

    feat(entities): add ExpenseCard component
    Added the ExpenseCard component along with its UI implementation. This component can display expense details such as description, date, and amount.
    
    This feature enhances the application by providing a reusable expense card component.

---
+++ src/entities/expense-card/index.ts
@@ -0,0 +1,1 @@
+export { Card as ExpenseCard } from "./ui/expense-card";
---
+++ src/entities/expense-card/ui/expense-card.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { ExpenseItem } from "../../../shared/lib/mock/dummy-expenses";
+import {
+  capitalizeFirstLetter,
+  colors,
+  fonts,
+  getFormattedDate,
+} from "../../../shared/lib";
+
+interface CardProps {
+  expense: ExpenseItem;
+}
+
+export const Card: React.FC<CardProps> = ({ expense }) => {
+  return (
+    <Pressable
+      style={({ pressed }) =>
+        pressed ? [styles.root, styles.pressed] : styles.root
+      }
+    >
+      <View>
+        <Text style={styles.description}>
+          {capitalizeFirstLetter(expense.description)}
+        </Text>
+        <Text style={styles.date}>{getFormattedDate(expense.date)}</Text>
+      </View>
+      <View style={styles.amountContainer}>
+        <Text style={styles.amount}>${expense.amount.toFixed(2)}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+    paddingVertical: 20,
+    borderRadius: 8,
+    backgroundColor: colors.primary[700],
+    marginVertical: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
+  },
+  pressed: {
+    opacity: 0.8,
+  },
+
+  description: {
+    fontFamily: fonts.gilroy800,
+    fontSize: 20,
+    color: colors.white,
+    lineHeight: 20,
+    marginBottom: 6,
+  },
+
+  date: {
+    fontFamily: fonts.roboto400,
+    fontSize: 17,
+    lineHeight: 17,
+    color: colors.white,
+  },
+  amountContainer: {
+    padding: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.white,
+    minWidth: 100,
+    borderRadius: 8,
+  },
+  amount: {
+    fontFamily: fonts.roboto700,
+    color: colors.primary[900],
+    fontSize: 17,
+    lineHeight: 17,
+  },
+});
