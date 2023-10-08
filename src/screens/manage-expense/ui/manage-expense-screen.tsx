bfa3c2e1634c1f105c2db6cf0d9423886e33a563
TretyakWeb <tretyakbro@gmail.com>
2023-10-08 23:49:11 +0300

    feat(ui): add Button component and expose it in the shared UI module
    In this commit, a new Button component has been introduced to the shared UI module. This Button component allows for the creation of customizable buttons with optional icons. It provides two modes: 'error' and 'flat', which can be used for different styling purposes.
    
    The Button component accepts the following props:
    - `children`: Text to display on the button.
    - `onPress`: Callback function to execute when the button is pressed.
    - `mode` (optional): Specifies the button mode ('error' or 'flat').
    - `iconName` (optional): Specifies the name of the icon to display on the button.
    - `iconColor` (optional): Specifies the color of the icon.
    - `iconSize` (optional): Specifies the size of the icon.
    
    This enhancement adds more flexibility to button creation in the application.

--- src/screens/manage-expense/ui/manage-expense-screen.tsx
+++ src/screens/manage-expense/ui/manage-expense-screen.tsx
@@ -1,10 +1,63 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { RouteProp } from "@react-navigation/native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+import { DUMMY_EXPENSES } from "../../../shared/lib/mock/dummy-expenses";
+import { capitalizeFirstLetter, colors, fonts } from "../../../shared/lib";
+import { Button, IconButton } from "../../../shared/ui";
+
+export type RootStackParamList = {
+  ManageExpense: { expenseId: string };
+};
 
 export const Screen: React.FC = () => {
+  const navigate = useNavigation();
+  const route = useRoute<RouteProp<RootStackParamList>>();
+
+  const expenseId: string = route.params?.expenseId;
+  const isEditing: boolean = Boolean(expenseId);
+
+  const expenseItem = DUMMY_EXPENSES.find((item) => item.id === expenseId);
+
+  useLayoutEffect(() => {
+    navigate.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigate, isEditing]);
+
   return (
-    <View>
-      <Text>ManageExpenseScreen</Text>
+    <View style={styles.root}>
+      {isEditing && expenseItem && (
+        <View style={styles.inner}>
+          <View>
+            <Text style={styles.description}>
+              {capitalizeFirstLetter(expenseItem.description)}
+            </Text>
+            <Text style={styles.amount}>${expenseItem.amount}</Text>
+          </View>
+          <Button onPress={() => {}}>Delete expense</Button>
+        </View>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    padding: 25,
+  },
+  inner: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  description: {
+    fontFamily: fonts.gilroy800,
+    fontSize: 18,
+  },
+  amount: {
+    fontFamily: fonts.gilroy300,
+    fontSize: 24,
+  },
+});
