dcc9f97caff4b88d125d9ac5481a4c12bb2ddffa
TretyakWeb <tretyakbro@gmail.com>
2023-10-11 23:50:45 +0300

    feat(redux): introduce Redux hooks for easier state management

---
+++ src/shared/lib/redux/hooks.ts
@@ -0,0 +1,6 @@
+import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = useDispatch<AppDispatch>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
