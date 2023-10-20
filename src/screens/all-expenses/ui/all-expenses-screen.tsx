592e19ddd423928b14ae04d10c8071d2c7427e66
TretyakWeb <tretyakbro@gmail.com>
2023-10-21 02:37:55 +0300

    feat(expense): fetch and display expenses from Redux store
    In this commit, I've updated the All Expenses screen to fetch and display expenses from the Redux store, ensuring that the data comes directly from the store. This change improves data consistency and prepares the screen for dynamic updates.

--- src/screens/all-expenses/ui/all-expenses-screen.tsx
+++ src/screens/all-expenses/ui/all-expenses-screen.tsx
@@ -1,13 +1,22 @@
-import React from "react";
-import { DUMMY_EXPENSES } from "shared";
+import React, { useEffect } from "react";
+import { DUMMY_EXPENSES, ExpenseItem, useAppDispatch } from "shared";
 import { StyleSheet, View } from "react-native";
 import { ExpenseList, ExpenseSummary } from "widgets";
+import { expenseModel } from "entities";
 
 export const Screen: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(expenseModel.actionsExpense.setExpenses(DUMMY_EXPENSES));
+  }, [DUMMY_EXPENSES, dispatch]);
+
+  const expenses: ExpenseItem[] = expenseModel.selectors.getExpenses();
+
   return (
     <View style={styles.root}>
       <ExpenseSummary expensesPeriod="Total" expenses={DUMMY_EXPENSES} />
-      <ExpenseList expenses={DUMMY_EXPENSES} />
+      <ExpenseList expenses={expenses} />
     </View>
   );
 };
