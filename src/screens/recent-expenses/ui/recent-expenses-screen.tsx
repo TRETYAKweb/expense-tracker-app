ad2ebd7ade2674cd63bd71ba98f1bacc3aabb439
TretyakWeb <tretyakbro@gmail.com>
2023-10-21 02:39:19 +0300

    feat(expense): fetch and display recent expenses from Redux store

--- src/screens/recent-expenses/ui/recent-expenses-screen.tsx
+++ src/screens/recent-expenses/ui/recent-expenses-screen.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import { ExpenseList, ExpenseSummary } from "widgets";
-import { DUMMY_EXPENSES } from "shared";
+import { DUMMY_EXPENSES, ExpenseItem, useAppDispatch } from "shared";
+import { expenseModel } from "entities";
 
 export const Screen: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(expenseModel.actionsExpense.setExpenses(DUMMY_EXPENSES));
+  }, [DUMMY_EXPENSES, dispatch]);
+
+  const expenses: ExpenseItem[] = expenseModel.selectors
+    .getExpenses()
+    .slice(0, 7);
+
   return (
     <View style={styles.root}>
-      <ExpenseSummary
-        expensesPeriod="Last 7 Days"
-        expenses={DUMMY_EXPENSES.slice(0, 7)}
-      />
-      <ExpenseList expenses={DUMMY_EXPENSES.slice(0, 7)} />
+      <ExpenseSummary expensesPeriod="Last 7 Days" expenses={expenses} />
+      <ExpenseList expenses={expenses} />
     </View>
   );
 };
