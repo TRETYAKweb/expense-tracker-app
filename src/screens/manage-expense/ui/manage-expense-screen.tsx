f5ca34bb89bab28fad9c18a57740cea91f7bd5d4
TretyakWeb <tretyakbro@gmail.com>
2023-10-23 10:05:20 +0300

    feat(manage-expense): Integrate Redux for expense management
    Integrate Redux store for managing expenses on the Manage Expense screen.
    - Retrieve expenses using useAppSelector.
    - Depending on the action (editing or adding), render UpdateExpense or AddExpense components.
    - Provide sample data for adding new expenses.

--- src/screens/manage-expense/ui/manage-expense-screen.tsx
+++ src/screens/manage-expense/ui/manage-expense-screen.tsx
@@ -2,10 +2,18 @@ import React, { useLayoutEffect } from "react";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { RouteProp } from "@react-navigation/native";
 import { StyleSheet, Text, View } from "react-native";
-
-import { DUMMY_EXPENSES } from "shared";
-import { capitalizeFirstLetter, colors, fonts } from "shared";
-import { Button } from "shared";
+import {
+  capitalizeFirstLetter,
+  colors,
+  fonts,
+  Button,
+  ExpenseItem,
+  useAppSelector,
+  mockData,
+  getFormattedDate,
+} from "shared";
+import { DeleteExpense, AddExpense, UpdateExpense } from "features";
+import { expenseModel } from "entities";
 
 export type RootStackParamList = {
   ManageExpense: { expenseId: string };
@@ -13,12 +21,15 @@ export type RootStackParamList = {
 
 export const Screen: React.FC = () => {
   const navigate = useNavigation();
+  const expenses: ExpenseItem[] = useAppSelector(
+    (state) => state.expense.expenses
+  );
   const route = useRoute<RouteProp<RootStackParamList>>();
 
   const expenseId: string = route.params?.expenseId;
   const isEditing: boolean = Boolean(expenseId);
 
-  const expenseItem = DUMMY_EXPENSES.find((item) => item.id === expenseId);
+  const expenseItem = expenses.find((item) => item.id === expenseId);
 
   useLayoutEffect(() => {
     navigate.setOptions({
@@ -26,13 +37,30 @@ export const Screen: React.FC = () => {
     });
   }, [navigate, isEditing]);
 
+  //TODO эта дата на время | UpdateExpense!
+
+  const data: Partial<ExpenseItem> = {
+    description: "Test",
+    amount: 99.99,
+  };
+
   return (
     <View style={styles.root}>
       <View style={styles.btnContainer}>
         <Button mode="flat" onPress={() => navigate.goBack()}>
           Cancle
         </Button>
-        <Button onPress={() => {}}>{isEditing ? "Update" : "Add"}</Button>
+        {isEditing ? (
+          <UpdateExpense
+            data={{ id: expenseId, data }}
+            updateExpense={expenseModel.actionsExpense.updateExpense}
+          />
+        ) : (
+          <AddExpense
+            data={mockData}
+            addExpense={expenseModel.actionsExpense.addExpdense}
+          />
+        )}
       </View>
       {isEditing && expenseItem && (
         <View style={styles.inner}>
@@ -42,15 +70,10 @@ export const Screen: React.FC = () => {
             </Text>
             <Text style={styles.amount}>${expenseItem.amount}</Text>
           </View>
-          <Button
-            mode="error"
-            iconName="trash"
-            iconSize={21}
-            iconColor={colors.white}
-            onPress={() => {}}
-          >
-            Delete expense
-          </Button>
+          <DeleteExpense
+            id={expenseId}
+            onDeleteExpense={expenseModel.actionsExpense.deleteExpense}
+          />
         </View>
       )}
     </View>
