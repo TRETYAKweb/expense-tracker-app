0e3293fe124750d79113e0bdefbb828f341044a7
TretyakWeb <tretyakbro@gmail.com>
2023-10-26 10:58:04 +0300

    feat(expense): update createExpense and add isValidForm
    Refactored the `createExpense` function to accept an `inputValuesState` parameter for better type safety. Additionally, added an `isValidForm` function to check the validity of the form based on the `inputValuesState`.

--- src/features/expense/add-expense/model.ts
+++ src/features/expense/add-expense/model.ts
@@ -1,6 +1,6 @@
 import { ExpenseItem } from "shared";
 
-export const createExpense = (expenseData: Partial<ExpenseItem>) => {
+export const createExpense = (expenseData: inputValuesState) => {
   let amount =
     typeof expenseData.amount === "string" ? parseFloat(expenseData.amount) : 0;
   if (isNaN(amount)) amount = 0;
@@ -11,3 +11,21 @@ export const createExpense = (expenseData: Partial<ExpenseItem>) => {
     id: Math.floor(Math.random() * 1000000).toString(),
   };
 };
+
+export const isValidForm = (inputValues: inputValuesState) => {
+  const isValidAmount = inputValues.amount > 0 && !isNaN(inputValues.amount);
+  const isValidDate = inputValues.date.length > 7;
+  const isValidDescription = inputValues.description.length > 0;
+
+  return {
+    isValidAmount,
+    isValidDate,
+    isValidDescription,
+  };
+};
+
+export interface inputValuesState {
+  amount: number;
+  description: string;
+  date: string;
+}
--- src/features/expense/update-expense/model.ts
+++ src/features/expense/update-expense/model.ts
@@ -10,3 +10,21 @@ export const createExpense = (expenseData: Partial<ExpenseItem>) => {
     amount: amount || 0,
   };
 };
+
+export const isValidForm = (inputValues: inputValuesState) => {
+  const isValidAmount = inputValues.amount > 0 && !isNaN(inputValues.amount);
+  const isValidDate = inputValues.date.length > 7;
+  const isValidDescription = inputValues.description.length > 0;
+
+  return {
+    isValidAmount,
+    isValidDate,
+    isValidDescription,
+  };
+};
+
+export interface inputValuesState {
+  amount: number;
+  description: string;
+  date: string;
+}
