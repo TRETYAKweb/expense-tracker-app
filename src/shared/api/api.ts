2ffc8930b87afe268ce7e737db9012808e112eec
TretyakWeb <tretyakbro@gmail.com>
2023-10-29 23:06:54 +0200

    feat(api): update expense API functions and add fetch method
    - Updated the expense API functions to enhance data handling.
    - Added a new fetch method to retrieve expense data from the server.
    - Transformed the response into an array of expense items.
    
    This change improves the overall handling of expense data and enables the retrieval of expenses from the server.

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
 import { API_URL } from "shared/constants";
+import { ExpenseItem } from "shared/lib";
 
-export interface IExpense {
-  amount: number;
-  description: string;
-  date: string;
-  id: string;
+interface IExpenseData {
+  [key: string]: ExpenseItem;
 }
 
 interface FirebaseResponse<T> {
@@ -14,9 +12,28 @@ interface FirebaseResponse<T> {
 
 export const api = Object.freeze({
   expense: {
-    create: async (data: IExpense): Promise<FirebaseResponse<string>> => {
-      const res = axios.post(`${API_URL}/expenses.json`, data);
-      return res;
+    fetch: async () => {
+      const response = await axios.get(`${API_URL}/expenses.json`);
+
+      const expenses: ExpenseItem[] = [];
+
+      for (const key in response.data) {
+        const expenseObj = {
+          id: key,
+          amount: response.data[key].amount,
+          date: response.data[key].date,
+          description: response.data[key].date,
+        };
+        expenses.push(expenseObj);
+      }
+
+      return expenses;
+    },
+    create: async (
+      data: ExpenseItem
+    ): Promise<FirebaseResponse<{ name: string }>> => {
+      const res = await axios.post(`${API_URL}/expenses.json`, data);
+      return res.data;
     },
   },
 });
