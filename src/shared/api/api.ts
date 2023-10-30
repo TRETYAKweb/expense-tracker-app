69f864d34ca82c4e4e049996398157ff168c0892
TretyakWeb <tretyakbro@gmail.com>
2023-10-30 23:13:21 +0200

    refactor(api): update expense endpoints
    - Cleaned up and optimized the expense endpoints in the API.
    - Removed unnecessary interfaces.
    - Added support for updating and deleting expenses.
    - Properly handled response data for create, update, and delete operations.

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,13 +2,9 @@ import axios from "axios";
 import { API_URL } from "shared/constants";
 import { ExpenseItem } from "shared/lib";
 
-interface IExpenseData {
-  [key: string]: ExpenseItem;
-}
-
-interface FirebaseResponse<T> {
-  data: T;
-}
+// interface FirebaseResponse<T> {
+//   data: T;
+// }
 
 export const api = Object.freeze({
   expense: {
@@ -22,18 +18,26 @@ export const api = Object.freeze({
           id: key,
           amount: response.data[key].amount,
           date: response.data[key].date,
-          description: response.data[key].date,
+          description: response.data[key].description,
         };
-        expenses.push(expenseObj);
+        expenses.unshift(expenseObj);
       }
 
       return expenses;
     },
-    create: async (
-      data: ExpenseItem
-    ): Promise<FirebaseResponse<{ name: string }>> => {
+    create: async (data: ExpenseItem) => {
       const res = await axios.post(`${API_URL}/expenses.json`, data);
-      return res.data;
+      return res;
+    },
+    update: async (id: string, updateData: ExpenseItem) => {
+      const response = await axios.put(
+        `${API_URL}/expenses/${id}.json`,
+        updateData
+      );
+      return response.data;
+    },
+    delete: async (id: string) => {
+      const response = await axios.delete(`${API_URL}/expenses/${id}.json`);
     },
   },
 });
