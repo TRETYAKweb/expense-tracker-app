d405d47e4dd8f733f15100f53e6f10df6a06b97b
TretyakWeb <tretyakbro@gmail.com>
2023-10-31 23:12:47 +0200

    feat: Add LoadingOverlay component for consistent loading states
    - Added a new "LoadingOverlay" component to provide a consistent loading state presentation.
    - The component displays an activity indicator.
    - Positioned the loading overlay to the center of the screen.

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,5 @@
 export * from "./icon-button/icon-button";
 export * from "./button/button";
 export * from "./input/input";
+export * from "./loading-overlay/loading-overlay";
+export * from "./window-notification/window-notification";
