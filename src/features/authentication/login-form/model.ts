b9a1f13b9f926c2a5d42920c07b18103edfb6c9b
TretyakWeb <tretyakbro@gmail.com>
2023-11-05 23:50:33 +0200

    feat(auth): add password validation to login form

--- src/features/authentication/login-form/model.ts
+++ src/features/authentication/login-form/model.ts
@@ -1,12 +1,14 @@
 import * as yup from "yup";
 
+export interface IFormData {
+  email: string;
+  password: string;
+}
+
 export const validationSchema = yup.object().shape({
   email: yup
     .string()
     .email("Email must be a valid email")
     .required("Email is required"),
-  confirmEmail: yup
-    .string()
-    .email("Email must be a valid email")
-    .oneOf([yup.ref("email")], "Email must match"),
+  password: yup.string().required("Password is required"),
 });
