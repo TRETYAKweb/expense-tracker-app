3c1cca6be98c982e2dcbc6f2f04b7b17fea167d9
TretyakWeb <tretyakbro@gmail.com>
2023-11-05 23:51:24 +0200

    refactor(ui): update shared input component

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -5,45 +5,30 @@ import {
   View,
   KeyboardTypeOptions,
   StyleSheet,
+  TextInputProps,
 } from "react-native";
+import { Control, Controller, FieldError } from "react-hook-form";
 import { colors, fonts } from "shared/lib";
 
-interface IInputConfig {
-  placeholder: string;
-  keyboardType: KeyboardTypeOptions;
-  onChangeText: (value: string) => void;
-  maxLength?: number;
-  value?: string;
-  multiline?: boolean;
-  numberOfLines?: number;
-  autoCapitalize?: "none" | "sentences" | "words" | "characters";
+interface InputProps {
+  label: string;
+  error?: FieldError | undefined;
+  inputProps?: TextInputProps;
 }
 
-interface inputProps {
-  lable: string;
-  inputConfig: IInputConfig;
-  style?: { flex: number };
-  isValid: boolean;
-}
-
-export const Input: React.FC<inputProps> = ({
-  lable,
-  inputConfig,
-  style,
-  isValid,
+export const Input: React.FC<InputProps> = ({
+  label,
+  error,
+  inputProps = {},
 }) => {
-  const lableStyle = [styles.lable, isValid && styles.errorLable];
-  const inputStyle = [
-    styles.input,
-    inputConfig.multiline && styles.multiline,
-    isValid && styles.errorInput,
-  ];
+  const lableStyle = [styles.lable];
+  const inputStyle = [styles.input, inputProps.multiline && styles.multiline];
 
   return (
-    <View style={[styles.root, style]}>
-      <Text style={lableStyle}>{lable}</Text>
-      <TextInput style={inputStyle} {...inputConfig} />
-      {isValid && <Text style={styles.textError}>Invalid input format.</Text>}
+    <View style={[styles.root, inputProps.style]}>
+      <Text style={lableStyle}>{label}</Text>
+      <TextInput {...inputProps} />
+      {error && <Text>{error.message}</Text>}
     </View>
   );
 };
