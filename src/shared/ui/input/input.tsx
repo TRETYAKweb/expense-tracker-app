08318d10b96107647a259e8b9872a750b89d2eca
TretyakWeb <tretyakbro@gmail.com>
2023-11-06 22:13:00 +0200

    style(ui): Improve Input component styles

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -3,32 +3,58 @@ import {
   Text,
   TextInput,
   View,
-  KeyboardTypeOptions,
   StyleSheet,
   TextInputProps,
 } from "react-native";
-import { Control, Controller, FieldError } from "react-hook-form";
+import { FieldError, Noop } from "react-hook-form";
 import { colors, fonts } from "shared/lib";
+
+type TypeMode = "light";
 
 interface InputProps {
   label: string;
   error?: FieldError | undefined;
   inputProps?: TextInputProps;
+  mode?: TypeMode;
+  onBlur: Noop;
+  value: string;
+  onChange: (text: string) => void;
+  style?: { flex: number };
 }
 
 export const Input: React.FC<InputProps> = ({
   label,
   error,
   inputProps = {},
+  mode,
+  value,
+  onBlur,
+  onChange,
+  style,
 }) => {
-  const lableStyle = [styles.lable];
-  const inputStyle = [styles.input, inputProps.multiline && styles.multiline];
+  const lableStyle = [
+    styles.lable,
+    error && styles.errorLable,
+    mode === "light" && styles.lightLabel,
+  ];
+  const inputStyle = [
+    styles.input,
+    inputProps.multiline && styles.multiline,
+    mode === "light" && styles.lightInput,
+    error && styles.errorInput,
+  ];
 
   return (
-    <View style={[styles.root, inputProps.style]}>
+    <View style={[styles.root, style]}>
       <Text style={lableStyle}>{label}</Text>
-      <TextInput {...inputProps} />
-      {error && <Text>{error.message}</Text>}
+      <TextInput
+        style={[inputStyle]}
+        value={value}
+        onChangeText={onChange}
+        onBlur={onBlur}
+        {...inputProps}
+      />
+      {error && <Text style={styles.textError}>{error.message}</Text>}
     </View>
   );
 };
@@ -43,6 +69,9 @@ const styles = StyleSheet.create({
     color: colors.primary[900],
     marginBottom: 7,
   },
+  lightLabel: {
+    color: colors.white,
+  },
   errorLable: {
     color: colors.error[500],
   },
@@ -54,9 +83,15 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 8,
   },
+  lightInput: {
+    borderColor: colors.white,
+    color: colors.white,
+  },
   errorInput: {
-    backgroundColor: colors.error[50],
     borderColor: colors.error[500],
+  },
+  placeholderTextColor: {
+    color: colors.error[500],
   },
   textError: {
     fontFamily: fonts.roboto400,
