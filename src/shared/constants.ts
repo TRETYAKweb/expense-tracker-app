22e6cd73bbcffb7b048705038ba2cbf6c4fbd53b
TretyakWeb <tretyakbro@gmail.com>
2023-11-07 20:47:44 +0200

    chore(constants): Add Firebase authentication URLs and API key constants
    - Added constants for Firebase signup and login URLs.
    - Introduced a constant for the Firebase API key.

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,1 +1,6 @@
 export const API_URL = process.env.EXPO_PUBLIC_API_URL;
+
+export const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
+
+export const SIGNUP_URL = process.env.EXPO_PUBLIC_FIREBASE_SIGNUP_URL;
+export const LOGIN_URL = process.env.EXPO_PUBLIC_FIREBASE_LOGIN_URL;
